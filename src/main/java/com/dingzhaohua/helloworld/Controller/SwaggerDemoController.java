aadfecb3ef4052a8a42357a5c863caa9356fef86
dzh1104 <zhding1104@163.com>
2018-11-13 15:59:49 +0800

    添加mysql连接

--- src/main/java/com/dingzhaohua/helloworld/Controller/SwaggerDemoController.java
+++ src/main/java/com/dingzhaohua/helloworld/Controller/SwaggerDemoController.java
@@ -3,10 +3,7 @@ package com.dingzhaohua.helloworld.Controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 // 路径
@@ -17,8 +14,13 @@ public class SwaggerDemoController {
     // 字段（id） 描述（用户ID）参数类型 是否必填 数据类型
     // dataType 应该要为 String，因为在swagger2页面中的输入框 拿到的是 字符串类型的，不修改为 String，页面中的输入框会报错
     @ApiImplicitParam(name = "id", value = "用户ID", paramType = "path", required = true, dataType = "String")
+
     // 路径 请求类型
-    @RequestMapping(value = "swagger2/{id}", method = RequestMethod.GET)
+    // @RequestMapping(value = "swagger2/{id}", method = RequestMethod.GET)
+
+    // 需要指定访问的方式，否则会导致其中一个影响就是swagger2显示所有的方法
+    @RequestMapping("swagger2/{id}")
+    // @GetMapping("swagger2/{id}")
     public String testSwagger2 (@PathVariable String id) {
         return id;
     }
