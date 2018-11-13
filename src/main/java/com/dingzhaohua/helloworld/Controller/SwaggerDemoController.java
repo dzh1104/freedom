b18ae0c04d1f4b1e469428efbeeed469114ef57c
dzh1104 <zhding1104@163.com>
2018-11-13 14:38:30 +0800

    参数类型应为String

--- src/main/java/com/dingzhaohua/helloworld/Controller/SwaggerDemoController.java
+++ src/main/java/com/dingzhaohua/helloworld/Controller/SwaggerDemoController.java
@@ -15,10 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class SwaggerDemoController {
     @ApiOperation(value = "测试swagger2的功能", notes = "使用swagger2的demo")
     // 字段（id） 描述（用户ID）参数类型 是否必填 数据类型
-    @ApiImplicitParam(name = "id", value = "用户ID", paramType = "path", required = true, dataType = "Integer")
+    // dataType 应该要为 String，因为在swagger2页面中的输入框 拿到的是 字符串类型的，不修改为 String，页面中的输入框会报错
+    @ApiImplicitParam(name = "id", value = "用户ID", paramType = "path", required = true, dataType = "String")
     // 路径 请求类型
     @RequestMapping(value = "swagger2/{id}", method = RequestMethod.GET)
-    public Integer testSwagger2 (@PathVariable int id) {
+    public String testSwagger2 (@PathVariable String id) {
         return id;
     }
 }
